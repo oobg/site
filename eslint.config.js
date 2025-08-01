029e3d694d8a3f39153579975de2c4452671db68
woong <yoonseok.bae98@gmail.com>
2025-08-01 20:29:17 +0900

    chore: refactor ESLint configuration to use consistent plugin imports

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,8 @@ import globals from "globals";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import importPlugin from "eslint-plugin-import";
+import eslintPluginReact from "eslint-plugin-react";
+import eslintPluginImport from "eslint-plugin-import";
 import tseslint from "typescript-eslint";
 import prettier from "eslint-config-prettier";
 
@@ -24,8 +25,8 @@ export default tseslint.config([
       globals: globals.browser,
     },
     plugins: {
-      react,
-      "import": importPlugin,
+      react: eslintPluginReact,
+      import: eslintPluginImport,
     },
     settings: {
       react: {
